683b4514fd099e78415ec550aac7d4cdf09f5d0b
Umm-e-Dua <160483094+Umm-e-Dua@users.noreply.github.com>
2024-06-23 01:38:38 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,4 @@
+// 19 Assignment
+let guestlist = ["laiba","sana", "qulshan", "mehrun nisa" ];
+let lengthGuests: number = guestlist.length;
+console.log(`We are inviting total ${lengthGuests} guests`);
